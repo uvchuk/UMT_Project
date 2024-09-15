b43a29b0a62d9e06f73ed5c2029b5e46e1110620
uvchuk <uvchuk@gmail.com>
2024-09-15 12:25:47 +0200

    buid UserTable component, import MUI datatable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UserTable from "./components/UserTable/UserTable";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <UserTable />
+    </div>
+  );
 }
 
 export default App;
